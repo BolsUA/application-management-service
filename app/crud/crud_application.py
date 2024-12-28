8ba823969b05aa838ef2779b090663eb537cf51e
PauloMaced0 <paulomacedo@ua.pt>
2024-12-28 02:19:33 +0000

    change router

--- app/crud/crud_application.py
+++ app/crud/crud_application.py
@@ -1,6 +1,9 @@
+import os
+import shutil
 from sqlalchemy.orm import Session
 from app.schemas import schemas
 from app.models import models
+from fastapi import HTTPException, UploadFile
 
 def create_application(db: Session, application: schemas.ApplicationBase):
     db_application = models.Application(
@@ -37,3 +40,53 @@ def create_document(db: Session, application_id: int, document_name: str, file_l
     db.commit()
     db.refresh(new_document)
     return new_document
+
+def create_application_document(db: Session, application_id: int, file: UploadFile) -> models.DocumentTemplate:
+    
+    document_name = get_filename_without_extension(file)
+
+    if not document_name:
+        raise HTTPException(status_code=400, detail="Document name could not be determined")
+
+    # Save the document file
+    file_location = ""
+    file_location = save_file(file, "application_files")
+    
+    if not file_location:
+        raise HTTPException(status_code=500, detail="Could not save document file")
+    
+    # Create the document template record
+    return create_document(db, application_id, document_name, file_location)
+
+def get_filename_without_extension(file: UploadFile) -> str:
+    if file is None or file.filename is None:
+        return None
+    # Split the filename into the name and extension
+    filename, _ = os.path.splitext(file.filename)
+    return filename
+
+def save_file(file: UploadFile, directory: str) -> str:
+    # Create the directory if it doesn't exist
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="File must have a valid filename.")
+
+    os.makedirs(directory, exist_ok=True)
+
+    # Sanitize the filename to prevent directory traversal attacks
+    filename = os.path.basename(file.filename)
+    if filename != file.filename or '..' in filename or filename.startswith('/'):
+        raise HTTPException(status_code=400, detail="Invalid filename.")
+
+    file_path = os.path.join(directory, filename)
+
+    print(filename)
+    try:
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+    except Exception:
+        raise HTTPException(status_code=500, detail="Could not save file.")
+
+    file_path = os.path.join(directory, file.filename)
+    # with open(file_path, "wb") as f:
+    #     f.write(file.file.read())
+    return file_path
--- app/routers/application.py
+++ app/routers/application.py
@@ -1,17 +1,14 @@
-import os
-import shutil
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
+from fastapi import APIRouter, Depends, UploadFile, File, Form
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.schemas import schemas
 from app.crud import crud_application
-import json
 
 router = APIRouter()
 
 # applications variable cant be a schemas because of the argument document_file...
-@router.post("/", response_model=schemas.ApplicationBase)
+@router.post("/applications", response_model=schemas.ApplicationBase)
 def create_application(
         scholarship_id: int = Form(...),
         user_id: str = Form(...),
@@ -36,69 +33,19 @@ def create_application(
 
     # Create the document records
     for document in  enumerate(document_file or []):
-        create_document(db, db_application.id, document[1])
+        crud_application.create_application_document(db, db_application.id, document[1])
 
     return db_application
 
-@router.get("/", response_model=list[schemas.ApplicationBase])
+@router.get("/applications", response_model=list[schemas.ApplicationBase])
 def get_applications(user_id: str, db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
     return crud_application.get_applications(db, user_id, skip, limit)
 
-@router.get("/{application_id}/details", response_model=schemas.ApplicationBase)
+@router.get("/applications/{application_id}/details", response_model=schemas.ApplicationBase)
 def get_application(application_id: int, db: Session = Depends(get_db)):
     return crud_application.get_application(db, application_id)
 
-@router.put("/{application_id}/status", response_model=schemas.ApplicationBase)
+@router.put("/applications/{application_id}/status", response_model=schemas.ApplicationBase)
 def update_application_status(application_id: int, status: schemas.ApplicationStatus, db: Session = Depends(get_db)):
     return crud_application.update_application_status(db, application_id, status)
 
-
-def create_document(db: Session, application_id: int, file: UploadFile) -> schemas.DocumentTemplate:
-    
-    document_name = get_filename_without_extension(file)
-
-    if not document_name:
-        raise HTTPException(status_code=400, detail="Document name could not be determined")
-
-    # Save the document file
-    file_location = ""
-    file_location = save_file(file, "application_files")
-    
-    if not file_location:
-        raise HTTPException(status_code=500, detail="Could not save document file")
-    
-    # Create the document template record
-    return crud_application.create_document(db, application_id, document_name, file_location)
-
-def get_filename_without_extension(file: UploadFile) -> str:
-    if file is None or file.filename is None:
-        return None
-    # Split the filename into the name and extension
-    filename, _ = os.path.splitext(file.filename)
-    return filename
-
-def save_file(file: UploadFile, directory: str) -> str:
-    # Create the directory if it doesn't exist
-    if not file.filename:
-        raise HTTPException(status_code=400, detail="File must have a valid filename.")
-
-    os.makedirs(directory, exist_ok=True)
-
-    # Sanitize the filename to prevent directory traversal attacks
-    filename = os.path.basename(file.filename)
-    if filename != file.filename or '..' in filename or filename.startswith('/'):
-        raise HTTPException(status_code=400, detail="Invalid filename.")
-
-    file_path = os.path.join(directory, filename)
-
-    print(filename)
-    try:
-        with open(file_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-    except Exception:
-        raise HTTPException(status_code=500, detail="Could not save file.")
-
-    file_path = os.path.join(directory, file.filename)
-    # with open(file_path, "wb") as f:
-    #     f.write(file.file.read())
-    return file_path
