b1b91026a9858ac5ee524e86c729073fe464e4ad
PauloMaced0 <paulomacedo@ua.pt>
2024-12-28 03:02:22 +0000

    rm env from main script

--- app/main.py
+++ app/main.py
@@ -8,16 +8,6 @@ from fastapi.staticfiles import StaticFiles
 from sqlmodel import SQLModel 
 from app.db.session import engine
 
-DATABASE_URL = str(os.getenv("DATABASE_URL", "sqlite:///todo.db"))
-SECRET_KEY = str(os.getenv('SECRET_KEY', 'K%!MaoL26XQe8iGAAyDrmbkw&bqE$hCPw4hSk!Hf'))
-REGION = str(os.getenv('REGION'))
-USER_POOL_ID = str(os.getenv('USER_POOL_ID'))
-CLIENT_ID = str(os.getenv('CLIENT_ID'))
-FRONTEND_URL = str(os.getenv('FRONTEND_URL'))
-
-# AWS Cognito configuration
-COGNITO_KEYS_URL = f'https://cognito-idp.{REGION}.amazonaws.com/{USER_POOL_ID}/.well-known/jwks.json'
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup event
